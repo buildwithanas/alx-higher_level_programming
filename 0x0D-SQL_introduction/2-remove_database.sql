7d65e34b5a13cffadc23aebea2f594f7c3f98ebe
anasamoo <anasamoo0704@gmail.com>
2026-08-04 20:04:53 +0100

    Add 0x0D-SQL_introduction

---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,5 @@
+-- Script that deletes the database hbtn_0c_0 in My MySQL server
+
+DROP DATABASE IF EXISTS
+hbtn_0c_0; 
+
---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,7 @@
+-- Script that creates a table called first_table in the current database in MySQL server
+CREATE TABLE first_table (
+	id INT,
+	NAME VARCHAR(256),
+	age INT
+);
+
