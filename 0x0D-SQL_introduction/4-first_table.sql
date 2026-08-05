9edd26d7e8119db3086bc191515988cce1ce5b51
anasamoo <anasamoo0704@gmail.com>
2026-08-05 21:50:20 +0100

    0x0D-SQL_introduction

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,7 +1,3 @@
--- Script that creates a table called first_table in the current database in MySQL server
-CREATE TABLE first_table (
-	id INT,
-	NAME VARCHAR(256),
-	age INT
-);
+-- Script that creates a table called first_table in the current database
+CREATE TABLE IF NOT EXISTS `first_table` (`id` INT, `name` VARCHAR(256));
 
